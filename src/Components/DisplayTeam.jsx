8bc041b53f3e67be192a3fbd82647c13f4858982
ayush amberkar <ayushempire03@gmail.com>
2024-03-15 17:23:32 +0530

    zero point functionalit y

---
+++ src/Components/DisplayTeam.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function DisplayTeam(props) {
+  //   const handleChange = (team) => {
+  //     props.handleOptionChange(team);
+  //   };
+
+  return (
+    <>
+      {/* {props.team.points > props.points ? (
+        <label className=" m-2">
+          <input
+            type="radio"
+            value={props.team.value}
+            checked={props.selectedTeam === props.team.value}
+            onChange={() => {
+              props.handleOptionChange(props.team);
+            }}
+          />{" "}
+          {props.team.name} {props.team.points}
+        </label>
+      ) : (
+        <></>
+      )} */}
+    </>
+  );
+}
+
+export default DisplayTeam;
