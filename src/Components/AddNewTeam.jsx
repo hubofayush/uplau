ee85747fe7a811b6aa150e130f78466844a8ffc2
ayush amberkar <ayushempire03@gmail.com>
2024-03-16 11:55:22 +0530

    added new Add new team component

---
+++ src/Components/AddNewTeam.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function AddNewTeam() {
+  return (
+    <>
+      <div>hello worold</div>
+    </>
+  );
+}
+
+export default AddNewTeam;
